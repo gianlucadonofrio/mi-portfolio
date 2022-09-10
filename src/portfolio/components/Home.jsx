a739d61db425cccc687c037633c8d2e8c87b0f34
gdonofrio <gian.donofrio2000@gmail.com>
2022-09-09 22:02:24 -0300

    Adding more files

--- src/portfolio/components/Home.jsx
+++ src/portfolio/components/Home.jsx
@@ -6,41 +6,14 @@ export const Home = () => {
       id="home"
       className="container-fluid min-vh-100 d-flex flex-column-reverse flex-md-row justify-content-end align-items-center"
     >
-      <div
-        className="container d-flex flex-column align-items-center justify-content-center d-none d-md-flex"
-        id="socials-desktop"
-        style={{ width: 'fit-content' }}
-      >
-        <a
-          href="https://www.linkedin.com/in/gianlucadonofrio/"
-          target={'_blank'}
-          className="btn-socials"
-          rel="noreferrer"
-        >
-          <i
-            className="fa-brands fa-linkedin p-1"
-            style={{ fontSize: '35px' }}
-          ></i>
-        </a>
-        <a
-          href="https://github.com/gianlucadonofrio"
-          target={'_blank'}
-          className="btn-socials"
-          rel="noreferrer"
-        >
-          <i
-            className="fa-brands fa-github p-1"
-            style={{ fontSize: '35px' }}
-          ></i>
-        </a>
-      </div>
       <div className="container d-flex flex-column-reverse justify-content-start align-items-center col-md-6 p-0">
         <div className="container d-flex justify-content-center w-100 flex-wrap">
           <a
-            href="https://drive.google.com/file/d/1AjktKDYo-TKXjJK4B9k8fREygwsy9sle/view?usp=sharing"
+            href={require('../../assets/CV-Gianluca-Donofrio.pdf')}
             target={'_blank'}
             rel="noreferrer"
             style={{ textDecoration: 'none' }}
+            download={'CV-Gianluca-Donofrio.pdf'}
           >
             <button
               className="btn btn__cv__contactme d-flex align-items-center justify-content-center"
@@ -61,34 +34,6 @@ export const Home = () => {
           </a>
         </div>
         <div className="d-flex flex-row align-items-center">
-          {/* <div
-            className="container d-flex flex-column d-sm-none p-0"
-            style={{ width: 'fit-content' }}
-            id="socials-mobile"
-          >
-            <a
-              href="https://www.linkedin.com/in/gianlucadonofrio/"
-              target={'_blank'}
-              className="btn-socials"
-              rel="noreferrer"
-            >
-              <i
-                className="fa-brands fa-linkedin p-1"
-                style={{ fontSize: '35px' }}
-              ></i>
-            </a>
-            <a
-              href="https://github.com/gianlucadonofrio"
-              target={'_blank'}
-              className="btn-socials"
-              rel="noreferrer"
-            >
-              <i
-                className="fa-brands fa-github p-1"
-                style={{ fontSize: '35px' }}
-              ></i>
-            </a>
-          </div> */}
           <div className="d-flex flex-column align-items-center justify-content-center">
             <h1 className="w-100 text-center ps-md-3 mt-2">
               Hola 👋🏼, soy
