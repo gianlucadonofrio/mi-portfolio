3bfc41c58a15a9a2c901ba60c58490987599d7ec
Gianluca D'Onofrio <giandonofrio@MBP-de-Gianluca.fibertel.com.ar>
2022-09-19 17:20:30 -0300

    adding files and editing about me section

--- src/portfolio/components/SkillsCards/SkillsCards.jsx
+++ src/portfolio/components/SkillsCards/SkillsCards.jsx
@@ -1,27 +1,36 @@
 export const SkillsCards = ({ languagePage }) => {
   return (
-    <div className="skills-cards mt-2 d-flex flex-column flex-md-row flex-wrap gap-2 justify-content-center">
+    <div className="skills-cards-container mt-2 d-flex flex-column flex-md-row flex-wrap gap-4 justify-content-center">
       {languagePage.skills__cards.map((card) => (
         <div
           key={card.id}
-          className="card skill-card-aboutme mt-3"
+          className="card skills-card-aboutme mt-3"
           style={{
-            backgroundColor: card.background_color,
+            backgroundColor: '#161b22',
             overflow: 'hidden',
+            boxShadow: '0 0 15px 0px var(--color-card-shadow)',
           }}
         >
           <img
             src={require(`../../../assets/skills/${card.img_code}`)}
-            alt=""
+            alt="skill"
             className="card-img-skills"
           />
           <div className="d-flex flex-column align-items-start justify-content-center mt-5">
             <i
               className={`fa-solid ${card.icon} ms-5`}
-              style={{ fontSize: '100px', color: 'var(  --color-navbar)' }}
+              style={{
+                fontSize: '100px',
+                color: 'var(--color-text-navbar)',
+                zIndex: '2',
+              }}
             ></i>
             <h5
-              style={{ fontSize: '22px', fontWeight: '600' }}
+              style={{
+                fontSize: '22px',
+                fontWeight: '600',
+                color: 'var(  --color-text-navbar)',
+              }}
               className="mt-3 ps-2"
             >
               {card.title}
@@ -36,11 +45,11 @@ export const SkillsCards = ({ languagePage }) => {
                   color: 'var(--color-text-navbar)',
                   fontWeight: '600',
                 }}
-                className="d-flex align-items-center mt-2"
+                className="d-flex align-items-center mt-3"
               >
                 <i
                   className="fa-solid fa-circle-check pe-2"
-                  style={{ color: '#45c4b0' }}
+                  style={{ color: 'var(--color-buttons)' }}
                 ></i>
                 {skill.skill}
               </li>
