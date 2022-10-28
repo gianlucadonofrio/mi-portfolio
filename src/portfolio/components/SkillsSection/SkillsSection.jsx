dfc5951b2ec9fb40085ad8231f3cf411e43b5a93
Gianluca D'Onofrio <giandonofrio@Gianlucas-MacBook-Pro.local>
2022-10-28 15:46:48 -0300

    Editing About me section

---
+++ src/portfolio/components/SkillsSection/SkillsSection.jsx
@@ -0,0 +1,71 @@
+export const SkillsSection = ({ languagePage }) => {
+  return (
+    <section>
+      <h3
+        className=" mt-4"
+        style={{
+          fontSize: '1.5rem',
+          fontWeight: '600',
+        }}
+      >
+        {languagePage.acerca_de_mi__title_habilidades}
+      </h3>
+      <div className="vbar"></div>
+      <div className="d-flex flex-wrap flex-column flex-md-row">
+        {languagePage.skills__cards.map((card) => (
+          <div
+            key={card.id}
+            className="d-flex col-md-6 mt-4  align-items-center justify-content-center gap-3"
+          >
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{
+                width: '25%',
+              }}
+            >
+              <i
+                className={`fa-solid ${card.icon}`}
+                style={{
+                  color: 'var(--color-buttons)',
+                  fontSize: '70px',
+                }}
+              ></i>
+            </div>
+            <div
+              className="d-flex flex-column"
+              style={{
+                width: '75%',
+              }}
+            >
+              <h5
+                style={{
+                  fontSize: '2rem',
+                  fontWeight: '600',
+                }}
+              >
+                {card.title}
+              </h5>
+              <p
+                style={{
+                  fontSize: '1.6rem',
+                }}
+              >
+                {card.skills.map((skill) => (
+                  <span
+                    key={skill.id}
+                    className="badge bg-secondary me-2 mt-2"
+                    style={{
+                      padding: '10px',
+                    }}
+                  >
+                    {skill.skill}
+                  </span>
+                ))}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
