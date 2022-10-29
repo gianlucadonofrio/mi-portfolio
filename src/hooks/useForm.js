475f8a5f8199fdc833a4c649a17dd5237c657638
Gianluca D'Onofrio <giandonofrio@Gianlucas-MBP.fibertel.com.ar>
2022-10-28 21:45:26 -0300

    aboutme section

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 export const useForm = (initalForm = {}) => {
   const [formState, setFormState] = useState(initalForm);
-  
+
   const onResetForm = () => {
     setFormState(initalForm);
   };
